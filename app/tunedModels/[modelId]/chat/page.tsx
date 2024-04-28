f3bd4a8a19da908db99396a8710e10ab9339c993
Nirlo <nirlo@yess.io>
2024-04-28 17:24:23 +0900

    FEAT: Model 페이지에서 현재 모델을 식별 가능하도록 표현

--- app/tunedModels/[modelId]/chat/page.tsx
+++ app/tunedModels/[modelId]/chat/page.tsx
@@ -4,6 +4,7 @@ import {nanoid} from '@/lib/utils'
 import {getMissingKeys} from '@/app/actions'
 import { AI } from '@/lib/chat/actions'
 import {Chat} from "@/components/chat";
+import {Badge} from "@/components/ui/badge";
 
 interface ModelPageProps {
   params: {
@@ -26,6 +27,22 @@ export default async function TunedModelPage({ params }: ModelPageProps) {
   const modelId = params.modelId;
   return (
     <>
+      <div className="mx-auto max-w-2xl">
+        <div className="flex flex-col gap-4 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
+          <h1>
+            <b>Tuned Model Test Page</b>
+          </h1>
+          <div className="flex gap-4">
+            <div className="text-sm text-zinc-600">
+              Model ID:
+              <Badge variant="secondary">
+                tunedModels/{params.modelId}
+              </Badge>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <AI initialAIState={{ chatId: id, interactions: [], messages: [], modelId }}>
         <Chat id={id} missingKeys={missingKeys} />
       </AI>
